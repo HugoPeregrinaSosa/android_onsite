27de11ffb45d0798a55bcf7434495c7c0698f6e8
Sergio Adrián Lagunas Pinacho <Shekomaru@Sergios-MacBook-Pro.local>
2016-06-03 13:15:20 -0500

    Avances hechos con single tweet

--- app/src/main/java/com/mvillasenor/twitter/SingleTweetActivity.java
+++ app/src/main/java/com/mvillasenor/twitter/SingleTweetActivity.java
@@ -1,20 +1,25 @@
 package com.mvillasenor.twitter;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 /**
  * Created by Shekomaru on 6/3/16.
  */
-public class SingleTweetActivity extends Activity {
+public class SingleTweetActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "SingleTweetActivity";
+
+    private Toolbar mToolbar;
+    private String tweetContent = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_singletweet);
 
-        String tweetContent = null;
         if (getIntent().getExtras() != null) {
             tweetContent = getIntent().getStringExtra("tweetId");
         }
@@ -23,8 +28,35 @@ public class SingleTweetActivity extends Activity {
             // TODO: 6/3/16 Load tweet data, and fill fields
             ((TextView) findViewById(R.id.tv_singletweet_content)).setText(tweetContent);
 
+            findViewById(R.id.bt_singletweet_share).setOnClickListener(this);
+
+            doSentimentAnalisys(tweetContent);
         }
 
         super.onCreate(savedInstanceState);
+    }
+
+    private void shareData(String tweetContent) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, tweetContent);
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.send_to)));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_singletweet_share:
+                if (tweetContent != null) {
+                    shareData(tweetContent);
+                }
+                break;
+        }
+
+    }
+
+    private void doSentimentAnalisys(String textToAnalyze) {
+        
     }
 }
